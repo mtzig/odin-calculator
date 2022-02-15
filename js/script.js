6e714d2c36fd614fea6a417204c69afd18bd252d
Thomas Zeng <tomzeng200@gmail.com>
2022-02-14 19:42:33 -0600

    added html buttons

---
+++ js/script.js
@@ -0,0 +1,34 @@
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b //need to make this handle divide by zero?
+}
+
+function operate(sign,a,b){
+    switch(sign){
+        case '+':
+            return add(a,b);
+            break;
+        case '-':
+            return subtract(a,b);
+            break;
+        case '*':
+            return multiply(a,b);
+            break;
+        case '/':
+            return divide(a,b);
+            break;
+        default:
+            return 0 //maybe some sort of error code
+    }
+}
