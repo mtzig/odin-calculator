7c0063ce5bd7b4e1692e508b61af836cc7d9bbe6
Thomas Zeng <tomzeng200@gmail.com>
2022-02-16 20:52:57 -0600

    added first rough implementation of ability to calculate

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 function add(a,b){
-    return a+b;
+    return +a+ +b;
 }
 
 function subtract(a,b){
-    return a-b;
+    return +a- +b;
 }
 
 function multiply(a,b){
-    return a*b;
+    return +a * +b;
 }
 
 function divide(a,b){
-    return a/b //need to make this handle divide by zero?
+    return +a / +b //need to make this handle divide by zero?
 }
 
 function operate(sign,a,b){
@@ -33,23 +33,69 @@ function operate(sign,a,b){
     }
 }
 
-const numButtons = document.querySelectorAll('.num');
-const opButtons = document.querySelectorAll('.op');
+// const numButtons = document.querySelectorAll('.num');
+const calcButtons = document.querySelectorAll('.op,.num,.equal');
 const currentDisplay = document.querySelector('.current');
+const prevDisplay = document.querySelector('.past-calc');
 const ACButton = document.querySelector('.ac');
+// const equalButton = document.querySelector('.equal');
+let firstVar = true;
+let op = false;
+let secondVar = false;
 
-function displayInput(e){
-    text = currentDisplay.textContent.concat(e.target.textContent)
-    currentDisplay.textContent = text.substr(0);//text.length-23);
+let a = ''
+let opSign = ''
+let b = ''
 
+
+function storeInput(e){
+    console.log(e.target.className);
+    if(e.target.className == 'num'){
+        if(firstVar){
+            a += e.target.textContent;
+            //display and break
+            currentDisplay.textContent = a;
+            
+        }else if(secondVar){
+            b += e.target.textContent;
+            currentDisplay.textContent = a+opSign+b;
+        }
+    } else if(e.target.className == 'op'){
+        if(!op && a != ''){
+            firstVar = false;
+            op = true;
+            secondVar = true;
+            opSign = e.target.textContent;
+            currentDisplay.textContent = a+opSign;
+            //display and break
+        }
+    } else if(e.target.className == 'equal'){
+        if(b != ''){ //only caculate if second var is present
+            op = false;
+            secondVar = false;
+
+            let answer = operate(opSign,a,b);
+            currentDisplay.textContent = answer;
+            prevDisplay.textContent = a+opSign+b+'=';
+
+        }
+        //do calculation and print
+    }
 }
+
+// function displayInput(e){
+//     text = currentDisplay.textContent.concat(e.target.textContent)
+//     currentDisplay.textContent = text.substr(0);//text.length-23);
+//     // console.log(e.target.className);
+
+// }
 
 ACButton.addEventListener('click',()=> currentDisplay.textContent='')
 
-numButtons.forEach(element => {
-    element.addEventListener('click', displayInput);
-});
+// numButtons.forEach(element => {
+//     element.addEventListener('click', storeInput);
+// });
 
-opButtons.forEach(element => {
-    element.addEventListener('click', displayInput);
+calcButtons.forEach(element => {
+    element.addEventListener('click', storeInput);
 });
